ee4f72d29bd1905f40d768498b32d6b20d1199ef
Md. Nazmul Alam <nazmulalamtuhin11011@gmail.com>
2024-05-09 17:30:16 +0600

    Comment out functions and Changed Event Loop

--- TASK101.cpp
+++ TASK101.cpp
@@ -5,19 +5,24 @@ const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char ** argv) {
 
+    // Window Event Initialization
     bool quit = false;
     SDL_Event event;
 
+    // Window and Renderer Initialization
     SDL_Window * window=NULL;
     SDL_Renderer * renderer=NULL;
 
+    // Initialization
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
 
+    // Main Loop
     int centerX = SCREEN_WIDTH/2;
     int centerY = SCREEN_HEIGHT/2;
     int radius = 100;
 
+    // Circle Drawing
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red color
     for (int w = 0; w < radius * 2; w++) {
         for (int h = 0; h < radius * 2; h++) {
@@ -29,16 +34,22 @@ int main(int argc, char ** argv) {
         }
     }
 
+    // Present Renderer
     SDL_RenderPresent(renderer);
 
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
+    // Event Loop
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            quit = true;
+            break;
         }
     }
 
+    // Clean Up
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
